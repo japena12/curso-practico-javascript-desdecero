63a70090f7aecabee637865e713d1765ae8bcc82
JOHN ALEXANDER PEÑA <japena12@gmail.com>
2024-02-14 19:47:42 -0500

    se agregan las carpetas de logos e iconos

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello, mundo")
+const menuemail = document.querySelector(".navbar-email");
+const desktopmenu = document.querySelector(".desktop-menu");
+
+menuemail.addEventListener("click",toggledesktopmenu);/* toggle para intercambiar */	
+
+function toggledesktopmenu(){
+  
+  desktopmenu.classList.toggle("inactive");/* permite intercambiar entre activo y inactivo */
+}
+console.log("funcionando");
