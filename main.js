9c68d6198b1634d792ef260e908a4fa88c76203e
JOHN ALEXANDER PEÑA <japena12@gmail.com>
2024-02-15 20:04:13 -0500

    se incluyo el menu mobile en el archivo general

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuemail = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu");
+const buhoicon = document.querySelector(".menu");
+const mobilemenu = document.querySelector(".mobile-menu");
 
 menuemail.addEventListener("click",toggledesktopmenu);/* toggle para intercambiar */	
+buhoicon.addEventListener("click",togglemobilemenu);
 
 function toggledesktopmenu(){
   
   desktopmenu.classList.toggle("inactive");/* permite intercambiar entre activo y inactivo */
+}
+
+function togglemobilemenu(){
+  
+  mobilemenu.classList.toggle("inactive");/* permite intercambiar entre activo y inactivo */
 }
